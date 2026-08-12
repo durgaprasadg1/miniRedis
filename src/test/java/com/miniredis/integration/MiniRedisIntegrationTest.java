7479ef2a0fb61e79212cde661905f7181837947d
Durgaprasad <durgaprasadgirepunje@gmail.com>
2026-08-12 22:03:41 +0530

    Implemented Test and Ran them

--- src/test/java/com/miniredis/integration/MiniRedisIntegrationTest.java
+++ src/test/java/com/miniredis/integration/MiniRedisIntegrationTest.java
@@ -52,4 +52,50 @@ public class MiniRedisIntegrationTest {
             serverThread.join(1000);
         }
     }
+
+    @Test
+    void shouldExecuteSetAndGet() throws Exception {
+
+        MiniRedisServer server = new MiniRedisServer(6380, 4);
+
+        Thread serverThread = new Thread(() -> {
+            try {
+                server.start();
+            } catch (IOException e) {
+            }
+        });
+
+        serverThread.start();
+
+        Thread.sleep(200);
+
+        try (
+                Socket socket = new Socket("localhost", 6380);
+
+                BufferedReader reader = new BufferedReader(
+                        new InputStreamReader(
+                                socket.getInputStream()));
+
+                PrintWriter writer = new PrintWriter(
+                        socket.getOutputStream(),
+                        true)) {
+
+            writer.println("SET name durga");
+
+            assertEquals(
+                    "OK",
+                    reader.readLine());
+
+            writer.println("GET name");
+
+            assertEquals(
+                    "durga",
+                    reader.readLine());
+
+        } finally {
+
+            server.stop();
+            serverThread.join(1000);
+        }
+    }
 }
